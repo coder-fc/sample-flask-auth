fbfa701e034b8fa90db5863aa6d1bd26987588e4
DevFC-M <felipech3344@gmail.com>
2024-03-20 23:15:12 -0300

    pdating (app.py, user.py) and adding database.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "my_secret_key"
-app.config['SQL_ALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
     return "Hello world"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
